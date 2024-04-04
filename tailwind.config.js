20e5f6be558c08e24c0474dd5753fa00c6c8bccc
Festus <techarenaz01@gmail.com>
2024-04-04 08:24:03 -0700

    commit 3

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,22 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
+    screens: {
+      'sm': '375px',
+      // => @media (min-width: 375px) { ... }
+
+      'md': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'lg': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1280px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1536px',
+      // => @media (min-width: 1536px) { ... }
+    },
     extend: {
       backgroundImage: {
         'hero-pattern': "url('/../img/sofa.jpg')",
