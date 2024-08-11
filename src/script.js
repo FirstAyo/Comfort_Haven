7e3da5ab3799032a6ccbb228f3e9ec9a882f7824
Festus <techarenaz01@gmail.com>
2024-08-10 23:07:14 -0700

    update to html and css

--- src/script.js
+++ src/script.js
@@ -2,12 +2,6 @@ const toggleMenu = document.getElementById ("toggle-menu");
 const mobileMenu = document.querySelector (".nav-links");
 
 function onToggle(e){
-    if (e.name === 'menu-outline'){
-        e.name = 'close-outline';
-        mobileMenu.classList.toggle('top-[12%]');
-    }
-    else{
-        e.name = 'menu-outline';
-        mobileMenu.classList.toggle('top-[-100%]');
-    }
+    e.name = e.name === 'menu-outline' ? 'close-outline' : 'menu-outline';
+    mobileMenu.classList.toggle('top-[10%]');
 }
