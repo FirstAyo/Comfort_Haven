e45de5e91809616a19f8a08c98e97dcde416bc7b
Festus <techarenaz01@gmail.com>
2025-01-14 11:18:15 -0800

    made the harmbuger menu functional,, added grid cols to md screens and adjusted the hero section for md screens

--- src/script.js
+++ src/script.js
@@ -1,7 +1,9 @@
-const toggleMenu = document.getElementById ("toggle-menu");
-const mobileMenu = document.querySelector (".nav-links");
+const toggleMenu = document.getElementById("toggle-menu");
+const mobileMenu = document.getElementById("nav-links");
 
 function onToggle(e){
     e.name = e.name === 'menu-outline' ? 'close-outline' : 'menu-outline';
-    mobileMenu.classList.toggle('top-[10%]');
+    mobileMenu.classList.toggle('top-[80px]');
 }
+
+// toggleMenu.addEventListener('click', onToggle());
